4fda655eca21cd577c28cfd3e3678c371ef2bcc0
julesandrea <julesandreatimadji@gmail.com>
2025-11-19 23:41:17 +0100

    Classe pour la variante

---
+++ src/projestJest/Variante.java
@@ -0,0 +1,35 @@
+package projestJest;
+
+public class Variante {
+	
+	    private String nom;
+
+	    private String description;
+
+	public void appliquerReglesSpecifiques() {
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
