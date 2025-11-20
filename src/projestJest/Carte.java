034fc5573e53ff80d53efbd1a57bd0d1ed57a4b9
julesandrea <julesandreatimadji@gmail.com>
2025-11-21 00:16:00 +0100

    Classe carte

---
+++ src/projestJest/Carte.java
@@ -0,0 +1,55 @@
+package projestJest;
+
+public class Carte {
+	
+	public ValeurCarte getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(ValeurCarte valeur) {
+		this.valeur = valeur;
+	}
+
+	public SuiteCarte getSuite() {
+		return suite;
+	}
+
+	public void setSuite(SuiteCarte suite) {
+		this.suite = suite;
+	}
+
+
+
+	private ValeurCarte valeur;
+	private SuiteCarte suite;
+	
+	/**
+	 * Constructeur pour une carte normale
+	 * @param valeur
+	 * @param suite
+	 */
+	public Carte(ValeurCarte valeur, SuiteCarte suite) {
+		this.valeur = valeur;
+		this.suite = suite;
+	}
+	
+	/**
+	 * Constructeur pour une carte Joker
+	 * @param valeur
+	 */
+	public Carte(ValeurCarte valeur) {
+	    if (valeur != ValeurCarte.JOKER) {
+	        throw new IllegalArgumentException("Ce constructeur est réservé aux Jokers");
+	    }
+	    this.valeur = valeur;
+	    this.suite = null; // Joker n'a pas de couleur
+	}
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
